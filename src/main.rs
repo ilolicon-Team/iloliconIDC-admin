abb35b9295c9edc150905144b803719649148a38
Ella Alinda <admin@ilolicon.com>
2024-03-01 21:21:17 +0800

    Axum: init

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
+mod handler;
+mod model;
+mod response;
+mod route;
 mod check;
 
-fn main() {
+use axum::http::{
+    header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE},
+    HeaderValue, Method,
+};
+use route::create_router;
+use tower_http::cors::CorsLayer;
+
+#[tokio::main]
+async fn main() {
     let mut args = std::env::args();
     // 跳过程序名称
     args.next();
@@ -14,14 +26,22 @@ fn main() {
                     println!("run 启动程序");
                 }
                 "install" => check::check_sqlite3(),
-                "run" => run(),
+                "run" => {
+                    let cors = CorsLayer::new()
+                        .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
+                        .allow_methods([Method::GET, Method::POST, Method::PATCH, Method::DELETE])
+                        .allow_credentials(true)
+                        .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE]);
+                
+                    let app = create_router().layer(cors);
+                
+                    println!("🚀 Server started successfully");
+                    let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
+                    axum::serve(listener, app).await.unwrap();
+                },
                 _ => println!("错误的参数"),
             }
         }
         None => println!("help 输出帮助信息"),
     }
-}
-
-fn run() {
-
 }
